59756cd4cda47a0e266d3d90bfb875c454c24e74
Joseph Yan'dev <joedevelopermiester@gmail.com>
2017-09-23 18:32:49 -0400

    Added textures for selection. Selections now change the texture of the tile!

--- objects/tile/Tile.py
+++ objects/tile/Tile.py
@@ -3,6 +3,8 @@ from panda3d.core import LPoint3f, PandaNode
 
 MODEL_FILE_PATH = "objects/tile/Tile.egg"
 MODEL_SCALE = LPoint3f(0.5, 0.5, 0.5)
+DEFAULT_TEXTURE_PATH = "objects/tile/Tile-Diffuse-Unselected.png"
+SELECTED_TEXTURE_PATH = "objects/tile/Tile-Diffuse-Selected.png"
 
 class Tile (GameObject):
     def __init__ (self, position, name="tile"):
@@ -11,18 +13,21 @@ class Tile (GameObject):
                             name)
 
         self.model.setScale(MODEL_SCALE)
-
         # Make sure our mouse raycasting can interact with this object:
         self.np.setPythonTag("selectable", self)
+
+        # Load textures we need (Automatically ignores pre-loaded):
+        self._defaultTex = base.loader.loadTexture(DEFAULT_TEXTURE_PATH)
+        self._selectTex = base.loader.loadTexture(SELECTED_TEXTURE_PATH)
+        # Set default texture:
+        self.model.setTexture(self._defaultTex)
 
     def setSelected (self, select=True):
         """
             Changes this tile's texture to be selected or deselected.
         """
         if select:
-            #TODO: Set selected texture
-            pass
+            self.model.setTexture(self._selectTex)
         else:
-            pass
-            #TODO: Revert selected texture
-        print ("Selected:", select)
+            # Revert to default:
+            self.model.setTexture(self._defaultTex)
