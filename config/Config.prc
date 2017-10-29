fd194a17e093c8a482014a96f9b05d4ccaf780a7
Joseph Yan'dev <joedevelopermiester@gmail.com>
2017-10-29 07:47:07 -0400

    Began work on MainMenu

--- config/Config.prc
+++ config/Config.prc
@@ -1,1 +1,7 @@
 window-title Teapot Wars 2
+#fullscreen #t
+win-size 1080 720
+#win-size 1920 1080
+show-frame-rate-meter true
+#Prevent user from resizing window at run-time:
+win-fixed-size 1
--- objects/gameManager/GameManager.py
+++ objects/gameManager/GameManager.py
@@ -2,16 +2,21 @@ from objects.localPlayer.PlayerController import PlayerController
 from objects.tileMap.TileMap import TileMap
 from panda3d.core import Point2D
 
+from direct.gui.DirectGui import DirectFrame
+from direct.gui.DirectGui import DirectButton
+
+TITLE_SCREEN_BACKGROUND_PATH = "objects/mainMenu/TitleScreen.png"
+
 class GameManager ():
     """
         Controls all game state and flow.
         This class has two main modes: a client mode, and a server mode.
     """
     def __init__ (self):
-        self.runTest()
+        pass
 
     def startMainMenu (self):
-        pass
+        _drawMainMenu()
 
     def runTest (self):
         self._tileMap = TileMap()
@@ -20,3 +25,23 @@ class GameManager ():
 
     def getTileMap (self):
         return self._tileMap
+
+def _drawMainMenu ():
+    """
+        Sets up main menu and draws GUI to screen.
+        Only supports aspect ratios >= 1 (where width is at least height)
+    """
+    # Set background color to black to cover any possible incorrect scaling:
+    #base.setBackgroundColor(0,0,0)
+    winWidth = base.getAspectRatio() * 2
+    backgroundFrame = DirectFrame(parent=base.a2dTopLeft,
+                                  frameSize=(0,winWidth, -2, 0),
+                                  frameTexture=TITLE_SCREEN_BACKGROUND_PATH)
+    b = DirectButton(parent=backgroundFrame,
+                     pos=(0, 0, 0),
+                     frameSize=(0, winWidth/2, -1, 0),
+                     text = ("Host Game"), )
+    b2 = DirectButton(parent=backgroundFrame,
+                     pos=(winWidth/2, 0, 0),
+                     frameSize=(0, winWidth/2, -1, 0),
+                     text = ("Host Game"), )
