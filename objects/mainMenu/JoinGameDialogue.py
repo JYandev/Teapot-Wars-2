a05b480888aed0dd2fd078dbb7c0cc15340275dc
jyandev <joedevelopermiester@gmail.com>
2017-11-10 13:17:34 -0500

    Added blockingFrame to the JoinGameDialogue draw function. It simply prevents clicks from going to the background buttons.

--- objects/mainMenu/JoinGameDialogue.py
+++ objects/mainMenu/JoinGameDialogue.py
@@ -13,26 +13,27 @@ class JoinGameDialogue ():
     def __init__ (self, confirmHandler):
         # Draw this GUI on object creation.
         self._confirmHandler = confirmHandler # The callback handler to this GUI
+        self._ipAddressEntry = None # Reference to the IP Address Text Entry
         self._draw()
-        self._ipAddress = DEFAULT_IP_ADDRESS
 
     def _draw (self):
         """
             Draws this JoinGameDialogue
         """
 
-        # Draw translucent background frame to block clicks:
-
-        # Draw Main Dialogue Frame:
         winWidth = base.getAspectRatio() * 2
         winHeight = 2
-        dFStartX, dFStartY = winWidth*(1/4), winHeight*(1/4)
-        dFSizeX, dFSizeY = winWidth/2 - dFStartX, winHeight/2 - dFStartY
+        # Draw translucent background frame to block clicks:
+        blockingFrame = DirectFrame(parent=base.a2dTopLeft,
+                                    frameSize=(0, winWidth, -winHeight, 0),
+                                    frameColor=(0,0,0,0.25))
+
+        # Draw Main Dialogue Frame:
+        dFStartX, dFStartY = winWidth*(1/5), winHeight*(1/6)
+        dFSizeX, dFSizeY = winWidth*(3/5), winHeight*(2/3)
         dialogueFrame = DirectFrame(parent=base.a2dTopLeft,
                                     pos=(dFStartX, 0, -dFStartY),
-                                    frameSize=(0,winWidth, -winHeight, 0))
-        # Draw Back button :
-        # Draw Connect button :
-
-    def _onIPEntrySet (self, textEntered):
-        self._ipAddress = textEntered
+                                    frameSize=(0, dFSizeX, -dFSizeY, 0))
+        # Draw Title Text:
+        # Draw Back Button:
+        # Draw Connect Button:
