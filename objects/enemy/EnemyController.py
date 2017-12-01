a062c5260aae0e7278e32af8491861676b0e04f8
Joseph Yan'dev <joedevelopermiester@gmail.com>
2017-12-01 15:11:10 -0500

    Added empty class for enemycontroller

---
+++ objects/enemy/EnemyController.py
@@ -0,0 +1,12 @@
+class EnemyController ():
+    """
+        Enemy controllers do exactly what there name implies - they act as the
+         AI controller for the NPCs.
+        Each enemy controller makes decisions on an interval which is randomly
+         chosen after a decision is made. Decisions are limited, of course, by
+         the character's energy level.
+        These controllers should only be running on the Host player's game.
+         the actions of these creatures should only be synced to the remote
+         clients.
+    """
+    pass
